e90a4c2b567d97d56af1e1010ca19e685756cf32
psachin <iclcoolster@gmail.com>
2012-09-12 18:41:30 +0530

    lot of bugs, added TODO

---
+++ insert-shebang.el
@@ -0,0 +1,37 @@
+(defun insert-shebang()
+  "whenever the new file is created, it will scan its extention
+and insert a shabang line"
+  (let ((bufname (buffer-name)))
+    ;;    (message "present buffer name: %s" bufname)
+    ;;    (message "present buffer extension: %s" (file-name-extension bufname)) 
+    (let ((extname (file-name-extension bufname)))
+      (if (equal extname "sh")
+	(progn
+	  (message "file is BASH")
+	  (goto-char (point-min))
+	  (insert-string "#!/bin/bash")
+	  )
+;;	(message "extension does not match")
+	(if (equal extname "py")
+	    (progn
+	      (message "file is PYTHON")
+	      (goto-char (point-min))
+	      (insert-string "#!/usr/bin/python")
+	      )
+	  (error "I cant identify the file extension")
+	  )
+	)
+      )
+    )
+  )
+
+(insert-shebang)
+
+(add-hook 'find-file-hook 'insert-shebang)
+
+;; TODO
+;; works only for new files
+;; what is shebang line already exist ??
+
+;; if the first line is not shebang, put it to the next line, and shebang will be the first line
+
