8d1ce2b738b8f61699883eeb089651ef006a43d7
psachin <iclcoolster@gmail.com>
2012-09-13 11:02:03 +0530

    nxt-replace(see docstring)

--- insert-shebang.el
+++ insert-shebang.el
@@ -1,6 +1,9 @@
 (defun insert-shebang()
   "whenever the new file is created, it will scan its extention
 and insert a shabang line"
+  (interactive "*")			;ASTERISK means - abort the
+					;function if the buffer is
+					;read-only
   (let ((bufname (buffer-name)))
     ;;    (message "present buffer name: %s" bufname)
     ;;    (message "present buffer extension: %s" (file-name-extension bufname)) 
@@ -55,12 +58,27 @@ and insert a shabang line"
     (end-of-line)
     (goto-char (point))
     (let ((pnt2 (point)))
-      ;; (message "%d,%d" pnt1 pnt2)
+      ;;(message "%d,%d" pnt1 pnt2)
       (buffer-substring-no-properties pnt1 pnt2)
-      )
+       )
     )
   ;;(point)
   )
 
 (testpoint)
+
+;; --------------------
+(defun nxt-replace()
+  "if the first line is not shebang, put it to the next line, and
+shebang will be the first line" 
+  (goto-char (point-min))
+  (kill-line)
+  (next-line)
+  (beginning-of-line)
+  (yank)
+  )
+
+(nxt-replace)
+
+
 
