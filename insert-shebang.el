edbd99febd96e4967950ca6b59e1161e62c27bba
psachin <iclcoolster@gmail.com>
2013-08-27 15:13:37 +0530

    Subject: file types like txt, org etc. can be ignored
    Description:
    
    1. added (defcostom ignore-extensions ...) to ignore files
    2. code indentation.

--- insert-shebang.el
+++ insert-shebang.el
@@ -41,39 +41,50 @@
   :type '(alist :key-type (string :tag "Extension") :value-type (string :tag "Type"))
   :group 'insert-shebang)
 
+(defcustom ignore-extensions
+  '("txt" "org")
+  "*Add extensions you want to ignore.
+List of file extensions to be ignored by default."
+  :type '(repeat (string :tag "extn"))
+  :group 'insert-shebang)
+
 (defun get-extension-and-insert (filename)
   "Get extension from FILENAME and insert shebang.
 FILENAME is a buffer name from which the extension in extracted."
   (interactive "*")
-    ;; strip filename extension
-    (setq file-extn (file-name-extension filename))
-    
-    ;; get value against the key
-    (if (car (assoc file-extn file-types))
-	;; if key exists in hashtable
-	(progn
-	  ;; set variable val to value of key
-	  (setq val (cdr (assoc file-extn file-types)))
-	  ;; if buffer is new
-	  (if (= (point-min) (point-max))
-	      (insert-in-current-buffer val)
-	    ;; if buffer has something, then
-	    (save-excursion
-	      (goto-char (point-min))
-	      ;; search for shebang pattern
-	      (if (integerp (re-search-forward "^#![ ]?\\([a-zA-Z_./]+\\)" 50 t))
-		  (message "This %s file already has shebang line" val)
-		;; prompt user
-		(if (y-or-n-p "File do not have shebang line, do you want to insert it now? ")
+  ;; strip filename extension
+  (setq file-extn (file-name-extension filename))
+  ;; check if this extension is ignored
+  (if (car (member file-extn ignore-extensions))
+      (progn (message "Extension ignored"))
+    ;; if not, check in extension list
+    (progn
+      ;; get value against the key
+      (if (car (assoc file-extn file-types))
+	  ;; if key exists in hashtable
+	  (progn
+	    ;; set variable val to value of key
+	    (setq val (cdr (assoc file-extn file-types)))
+	    ;; if buffer is new
+	    (if (= (point-min) (point-max))
+		(insert-in-current-buffer val)
+	      ;; if buffer has something, then
+	      (save-excursion
+		(goto-char (point-min))
+		;; search for shebang pattern
+		(if (integerp (re-search-forward "^#![ ]?\\([a-zA-Z_./]+\\)" 50 t))
+		    (message "This %s file already has shebang line" val)
+		  ;; prompt user
+		  (if (y-or-n-p "File do not have shebang line, do you want to insert it now? ")
+		      (progn
+			(insert-in-current-buffer val)
+			(message "Shebang inserted"))
 		    (progn
-		      (insert-in-current-buffer val)
-		      (message "Shebang inserted"))
-		  (progn
-		    (message "Leaving file as it is"))
-		  )))))
-      ;; if key don't exists
-      (progn
-	(message "Can't guess file type. Type: M-x customize-group RET insert-shebang"))))
+		      (message "Leaving file as it is"))
+		    )))))
+	;; if key don't exists
+	(progn
+	  (message "Can't guess file type. Type: 'M-x customize-group RET insert-shebang' to customize"))))))
 
 (defun insert-in-current-buffer(val)
   "Insert shebang in current buffer"
