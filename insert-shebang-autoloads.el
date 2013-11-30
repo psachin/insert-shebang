8a00e2cca1d8947cbbffb80744f7a37b93dab596
psachin <iclcoolster@gmail.com>
2013-11-30 11:24:45 +0530

    Subject: Clean command to remove *autoloads.el files in Makefile.
    Description:

--- insert-shebang-autoloads.el
+++
@@ -1,26 +0,0 @@
-;;; insert-shebang-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-
-;;;### (autoloads (insert-shebang) "insert-shebang" "insert-shebang.el"
-;;;;;;  (21122 28321))
-;;; Generated autoloads from insert-shebang.el
-
-(autoload 'insert-shebang "insert-shebang" "\
-Insert shebang line automatically.
-Calls function `insert-shebang-get-extension-and-insert'.  With argument as
-`buffer-name'.
-
-\(fn)" t nil)
-
-;;;***
-
-(provide 'insert-shebang-autoloads)
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; insert-shebang-autoloads.el ends here
